8a920a0b472b2724281662c28e27ba6494781266
Talhaasif92 <40741974+Talhaasif92@users.noreply.github.com>
2024-09-14 12:48:51 +0500

    Show Image In grid form

--- app/src/main/java/com/devfast/imagetask/screens/ApiImagesTab.kt
+++ app/src/main/java/com/devfast/imagetask/screens/ApiImagesTab.kt
@@ -1,9 +1,16 @@
 package com.devfast.imagetask.screens
 
 import android.util.Log
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -12,6 +19,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.devfast.imagetask.ImageViewModel
 import com.devfast.imagetask.networking.ImageApiClient
@@ -46,18 +56,32 @@ fun ApiImagesTab(imageViewModel: ImageViewModel) {
 
 @Composable
 fun ImageList(images: List<PhotosItem?>) {
-    LazyColumn {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = Modifier.padding(4.dp)
+    ) {
         items(images) { image ->
             image?.let {
-                AsyncImage(
-                    model = it.src?.landscape, // Access medium from Src
-                    contentDescription = it.alt,
-                    modifier = Modifier.fillMaxWidth()
-                )
+                Card(
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .fillMaxWidth(),
+                    elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+                ) {
+                    var isImageLoaded by remember { mutableStateOf(false) }
+                    AsyncImage(
+                        model = it.src?.medium,
+                        contentDescription = it.alt,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .aspectRatio(1f)
+                            .alpha(if (isImageLoaded) 1f else 0f), // Fade in effect
+                        contentScale = ContentScale.Crop,
+                        onSuccess = { isImageLoaded = true } // Image loaded
+                    )}
             }
         }
     }
 }
-
 
 
